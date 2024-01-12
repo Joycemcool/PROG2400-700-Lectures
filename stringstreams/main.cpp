b2c7e7c19a4983e9db187ebe0c978c8e991c4e37
Brian Shewan <brian.shewan@nscc.ca>
2024-01-12 12:10:36 -0400

    added pointers and dynamic array demos

--- stringstreams/main.cpp
+++ stringstreams/main.cpp
@@ -26,17 +26,26 @@ int main() {
         // try to extract a number
         ss >> start;
         if (ss.fail()) {
-            
+            std::cout << "Didn't have a starting number." << std::endl;
+
+            // check whether or not the entire input was a single letter
+            if (user_input.length() == 1) {
+                std::cout << "I think you entered a command." << std::endl;
+            } else {
+                std::cout << "You entered a sentence." << std::endl;
+            }
         } else {
+            std::cout << "There was a first number." << std::endl;
+
             // success! try to extract a second number
             ss >> end;
             if (ss.fail()) {
                 std::cout << "There was no ending number." << std::endl;
+            } else {
+                std::cout << "There was a second number." << std::endl;
             }
         }
     }
-
-    std::cin >> command >> start >> end;
 
     std::cout << "The command is: " << command << std::endl;
     std::cout << "The first number is: " << start << std::endl;
